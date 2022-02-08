938a2266894d2305556faefdb05e81ef7eb503ca
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-08 17:12:58 +0700

    Using The Custom Hook In More Components

--- my-react-app/src/components/hooks/use-http.js
+++ my-react-app/src/components/hooks/use-http.js
@@ -1,33 +1,36 @@
-import React, { useState } from "react";
+import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
+
       if (!response.ok) {
-        throw new Error("Request failed!");
+        throw new Error('Request failed!');
       }
+
       const data = await response.json();
       applyData(data);
-    } catch (error) {
-      setError(error.message || "Something went wrong!");
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, []);
+
   return {
-      isLoading,
-      error,
-      sendRequest,
-  }
+    isLoading,
+    error,
+    sendRequest,
+  };
 };
 
 export default useHttp;
